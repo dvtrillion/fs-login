505f5e09c1d0709409f30cc35e35844de2225966
dvtrillion <wbtrillion@gmail.com>
2022-12-08 12:58:06 +0900

    login oper

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -18,6 +18,8 @@ function login() {
             "content-Type": "application/json",
         },
         body: JSON.stringify(req),
-    });
+    })
+        .then((res) => res.json())
+        .then(console.log);
 
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -9,13 +9,34 @@ const output = {
         res.render("home/login");
     },
 
-}
+};
+
+const users = {
+    id: ["admin", "administor,"],
+    psword: ["1234", "qwe12qwe12!"],
+};
+
 
 const process = {
     login: (req, res) => {
-        console.log(req.body);
-    }
-}
+        const id = req.body.id,
+        psword = req.body.psword;
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+    
+        return res.json({
+            success: false,
+            msg: "아이디 또는 비밀번호를 확인하세요",
+        });
+    },
+};
 
 module.exports = {
     output,
